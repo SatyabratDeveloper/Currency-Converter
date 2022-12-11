7b26294037c669597d465b27d8f2bcf7f0d2bde9
SatyabratDeveloper <satyabrat.jha@vidyamantra.com>
2022-12-11 20:20:17 +0530

    Input component added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,10 @@
 <script setup lang="ts">
-import { useConverterStore } from './stores/converter'
-
-// defining store
-const converterStore = useConverterStore()
+import Input from './components/input.vue'
 </script>
 
 <template>
   <h1>Currency Converter</h1>
-
-    <!-- from country selector -->
-    <div>
-      <select id="from" v-model="converterStore.from">
-        <option v-for="country in converterStore.fromCountryList" :key="country.id" :value="country">{{ country.country }}</option>
-      </select>
-      <!-- from input -->
-      <input type="number" v-model="converterStore.fromAmount">
-    </div>
-
-    <!-- to country selector -->
-    <div>
-      <select id="to" v-model="converterStore.to">
-        <option v-for="country in converterStore.toCountryList" :key="country.id" :value="country">{{ country.country }}</option>
-      </select>
-      <!-- to input -->
-      <input type="number" v-model="converterStore.toAmount" :disabled="true">
-    </div>
-
-    <button @click="converterStore.currConverter">Convert</button>
+  <Input />
 </template>
 
 <style>
@@ -43,25 +21,5 @@ h1 {
   text-align: center;
   width: 100%;
 }
-form {
-  display: flex;
-  width: 700px;
-  margin: auto;
-}
-div {
-  margin: 5%;
-}
-select{
-  width: 105%;
-  height: 30px;
-  background-color: #eee;
-  font-size: 16px;
-  padding: 0 5px;
-}
-input {
-  width: 100%;
-  height: 40px;
-  padding: 0 5px;
-  font-size: 20px;
-}
+
 </style>
---
+++ src/components/input.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <select id="from" v-model="converterStore.from">
+      <option
+        v-for="country in converterStore.fromCountryList"
+        :key="country.id"
+        :value="country"
+      >
+        {{ country.country }}
+      </option>
+    </select>
+    <!-- from input -->
+    <input type="number" v-model="converterStore.fromAmount" />
+  </div>
+
+  <!-- to country selector -->
+  <div>
+    <select id="to" v-model="converterStore.to">
+      <option
+        v-for="country in converterStore.toCountryList"
+        :key="country.id"
+        :value="country"
+      >
+        {{ country.country }}
+      </option>
+    </select>
+    <!-- to input -->
+    <input type="number" v-model="converterStore.toAmount" :disabled="true" />
+  </div>
+
+  <button @click="converterStore.currConverter">Convert</button>
+</template>
+
+<script setup lang="ts">
+import { useConverterStore } from "@/stores/converter";
+
+// defining store
+const converterStore = useConverterStore();
+</script>
+
+<style scoped>
+div {
+  margin: 5%;
+}
+select{
+  width: 105%;
+  height: 30px;
+  background-color: #eee;
+  font-size: 16px;
+  padding: 0 5px;
+}
+input {
+  width: 100%;
+  height: 40px;
+  padding: 0 5px;
+  font-size: 20px;
+}
+</style>
